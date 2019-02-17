a10e877886389648068082dbbd69305977cfeb57
Sverre <sskodje@gmail.com>
2019-02-18 00:00:22 +0100

    bumped version to 4.1.0.0

--- src/Highlight/Properties/AssemblyInfo.cs
+++ src/Highlight/Properties/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
 [assembly: AssemblyInformationalVersion("4.0.0.0")]
